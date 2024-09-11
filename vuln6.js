b79e241f72ecea619ef2208832b1cb21b9b0c537
gumBalG <104804210+gumBalG@users.noreply.github.com>
2024-09-11 19:39:22 -0300

    fix-sugestion/vuln6.js-ARX

--- vuln6.js
+++ vuln6.js
@@ -1,23 +1,41 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
-var escape = require('escape-html');
-var serialize = require('node-serialize');
+var xss = require('xss');
 var app = express();
 app.use(cookieParser())
- 
+
 app.get('/', function(req, res) {
- if (req.cookies.profile) {
-   var str = new Buffer(req.cookies.profile, 'base64').toString();
-   var obj = serialize.unserialize(str);
-   if (obj.username) {
-     res.send("Hello " + escape(obj.username));
-   }
- } else {
-     res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
-       maxAge: 900000,
-       httpOnly: true
-     });
- }
- res.send("Hello World");
+  if (req.cookies.profile) {
+    try {
+      var str = Buffer.from(req.cookies.profile, 'base64').toString();
+      var obj = JSON.parse(str);
+      if (obj.username) {
+        // Use xss() function to sanitize the username
+        res.send("Hello " + xss(obj.username));
+      } else {
+        res.send("Hello Guest");
+      }
+    } catch (error) {
+      console.error('Error parsing profile:', error);
+      res.status(400).send("Invalid profile data");
+    }
+  } else {
+    var profile = {
+      username: "guest",
+      country: "unknown",
+      city: "unknown"
+    };
+    var encodedProfile = Buffer.from(JSON.stringify(profile)).toString('base64');
+    res.cookie('profile', encodedProfile, {
+      maxAge: 900000,
+      httpOnly: true,
+      secure: true, // Only send cookie over HTTPS
+      sameSite: 'strict' // Protect against CSRF
+    });
+    res.send("Hello Guest");
+  }
 });
-app.listen(3000);
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
