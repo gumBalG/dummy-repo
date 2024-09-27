3115dd1f90b861e519612fb0a8f8b25b501f5e29
gumBalG <104804210+gumBalG@users.noreply.github.com>
2024-09-26 21:46:56 -0300

    fix-suggestion/vuln6.js-ARX

--- vuln6.js
+++ vuln6.js
@@ -4,20 +4,23 @@ var escape = require('escape-html');
 var serialize = require('node-serialize');
 var app = express();
 app.use(cookieParser())
- 
+
 app.get('/', function(req, res) {
  if (req.cookies.profile) {
    var str = new Buffer(req.cookies.profile, 'base64').toString();
    var obj = serialize.unserialize(str);
    if (obj.username) {
-     res.send("Hello " + escape(obj.username));
+     // Use res.render() with a template engine instead of directly sending the response
+     // This example assumes you're using a template engine like EJS
+     res.render('greeting', { username: escape(obj.username) });
    }
  } else {
      res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
        maxAge: 900000,
        httpOnly: true
      });
+     res.send("Hello World");
  }
- res.send("Hello World");
 });
+
 app.listen(3000);
