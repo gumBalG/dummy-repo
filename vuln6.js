04e2c36721fcfba8a5ffa1b496d95790ead20cd4
gumBalG <104804210+gumBalG@users.noreply.github.com>
2024-09-29 15:30:39 -0300

    fix-suggestion/vuln6.js-ARX

--- vuln6.js
+++ vuln6.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
-var escape = require('escape-html');
 var serialize = require('node-serialize');
+var xss = require('xss');
 var app = express();
 app.use(cookieParser())
- 
+
 app.get('/', function(req, res) {
  if (req.cookies.profile) {
    var str = new Buffer(req.cookies.profile, 'base64').toString();
    var obj = serialize.unserialize(str);
    if (obj.username) {
-     res.send("Hello " + escape(obj.username));
+     // Use xss() function to sanitize the username
+     res.send("Hello " + xss(obj.username));
    }
  } else {
      res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
@@ -20,4 +21,5 @@ app.get('/', function(req, res) {
  }
  res.send("Hello World");
 });
+
 app.listen(3000);
