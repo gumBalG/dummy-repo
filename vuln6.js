3f1e948533c3d03d8924bdc55d98b2c0d7c8c22a
gumBalG <104804210+gumBalG@users.noreply.github.com>
2024-10-08 00:41:50 -0300

    fix-suggestion/vuln6.js-ARX

--- vuln6.js
+++ vuln6.js
@@ -1,23 +1,29 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var escape = require('escape-html');
-var serialize = require('node-serialize');
 var app = express();
 app.use(cookieParser())
- 
+
 app.get('/', function(req, res) {
  if (req.cookies.profile) {
-   var str = new Buffer(req.cookies.profile, 'base64').toString();
-   var obj = serialize.unserialize(str);
-   if (obj.username) {
-     res.send("Hello " + escape(obj.username));
+   try {
+     var str = Buffer.from(req.cookies.profile, 'base64').toString();
+     var obj = JSON.parse(str);
+     if (obj.username && typeof obj.username === 'string') {
+       res.send("Hello " + escape(obj.username));
+     } else {
+       throw new Error('Invalid username');
+     }
+   } catch (error) {
+     res.status(400).send("Invalid profile data");
    }
  } else {
      res.cookie('profile', "eyJ1c2VybmFtZSI6ImFqaW4iLCJjb3VudHJ5IjoiaW5kaWEiLCJjaXR5IjoiYmFuZ2Fsb3JlIn0=", {
        maxAge: 900000,
        httpOnly: true
      });
+     res.send("Hello World");
  }
- res.send("Hello World");
 });
+
 app.listen(3000);
